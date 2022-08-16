2f151578badeb9742d6d4ab65cb91c55aeec3214
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-16 12:37:52 +0700

    added many markers

--- app/src/main/java/com/asthiseta/mygooglemaps/MapsActivity.kt
+++ app/src/main/java/com/asthiseta/mygooglemaps/MapsActivity.kt
@@ -21,11 +21,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.asthiseta.mygooglemaps.databinding.ActivityMapsBinding
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -91,8 +88,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-        getMyLocation()
-
         val dicodingSpace = LatLng(-6.8957643, 107.6338462)
         mMap.addMarker(
             MarkerOptions()
@@ -119,6 +114,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     .icon(vectorToBitmap(R.drawable.ic_custom_pin, Color.parseColor("#3DDC84")))
             )
         }
+
+        getMyLocation()
+        addManyMarker()
+    }
+
+    private val boundsBuilder = LatLngBounds.Builder()
+
+    private fun addManyMarker() {
+        val tourismPlace = listOf(
+            TourismPlace("Floating Market Lembang", -6.8168954,107.6151046),
+            TourismPlace("The Great Asia Africa", -6.8331128,107.6048483),
+            TourismPlace("Rabbit Town", -6.8668408,107.608081),
+            TourismPlace("Alun-Alun Kota Bandung", -6.9218518,107.6025294),
+            TourismPlace("Orchid Forest Cikole", -6.780725, 107.637409),
+        )
+        tourismPlace.forEach { tourism ->
+            val latLng = LatLng(tourism.latitude, tourism.longitude)
+            mMap.addMarker(MarkerOptions().position(latLng).title(tourism.name))
+            boundsBuilder.include(latLng)
+        }
+
+        val bounds: LatLngBounds = boundsBuilder.build()
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds,
+                resources.displayMetrics.widthPixels,
+                resources.displayMetrics.heightPixels,
+                300
+            )
+        )
     }
 
     private fun vectorToBitmap(@DrawableRes id: Int, @ColorInt color: Int): BitmapDescriptor {
---
+++ app/src/main/java/com/asthiseta/mygooglemaps/TourismPlace.kt
@@ -0,0 +1,7 @@
+package com.asthiseta.mygooglemaps
+
+data class TourismPlace(
+    val name: String,
+    val latitude : Double,
+    val longitude: Double
+)
